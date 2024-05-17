57d4375221d54da6124fa40339e815b713e3bee2
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-05-17 14:10:28 +0900

    Initial Commit

---
+++ 24.05.17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  24.05.17
+//
+//  Created by 이윤지 on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
