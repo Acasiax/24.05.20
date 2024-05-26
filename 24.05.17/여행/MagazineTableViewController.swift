9af216af45b0ea6f0969a90796cffe1a513571ec
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-05-26 15:44:11 +0900

    여행 네비게이션뷰 추기

---
+++ 24.05.17/여행/MagazineTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  MagazineTableViewController.swift
+//  24.05.17
+//
+//  Created by 이윤지 on 5/26/24.
+//
+
+import UIKit
+
+class MagazineTableViewController: UITableViewController {
+
+    // 잡지 정보를 담은 배열
+       var magazineInfoList: [Magazine] = MagazineInfo().magazine
+       
+       override func viewDidLoad() {
+           //🌟뷰컨트롤러 안에 뷰디드로드가 있어서 super로 호출(면접출제)
+           super.viewDidLoad()
+           
+           // 테이블 뷰 설정하기
+          // tableView.separatorStyle = .singleLine // 구분선
+           tableView.rowHeight = UITableView.automaticDimension // 자동 오토 높이 설정! 코드 기억해두기~
+           tableView.estimatedRowHeight = 450 // 예상 높이 설정
+           
+       }
+       
+    // 🌟뷰가 나타날 때마다 호출되는 메소드
+       override func viewWillAppear(_ animated: Bool) {
+           super.viewWillAppear(animated)
+           
+           // 데이터를 다시 불러오거나 업데이트가 필요한 경우
+           // reloadData() 메소드를 호출하여 테이블 뷰를 갱신하는 코드임!
+           tableView.reloadData()
+           print("🥕viewWillAppea 호출, reloadData 갱신")
+       }
+       
+    
+    
+       // 섹션 내 행의 개수 반환하는 코드
+       override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return magazineInfoList.count
+       }
+       
+       // 각 행에 대한 셀 설정하기
+       override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           //수업때 배운 가드문 사용하여 셀 초기화 하기
+           guard let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineCell", for: indexPath) as? TravelTableCell else {
+               return UITableViewCell()
+           }
+           let magazine = magazineInfoList[indexPath.row]
+           cell.SetUpconfigure(title: magazine.title, subtitle: magazine.subtitle, date: magazine.date, imageUrl: magazine.photo_image)
+           
+           return cell
+       }
+       
+       // 행 선택 시 호출되는 메소드
+       override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+           print(indexPath)
+           guard let url = URL(string: magazineInfoList[indexPath.row].link) else { return }
+           
+           // 선택된 링크 열기
+           UIApplication.shared.open(url, options: [:], completionHandler: nil)
+       }
+   }
---
+++ 24.05.17/여행/TravelTableCell.swift
@@ -0,0 +1,54 @@
+//
+//  MagazineTableViewCell.swift
+//  24.05.17
+//
+//  Created by 이윤지 on 5/26/24.
+//
+
+import UIKit
+import Kingfisher
+
+class TravelTableCell: UITableViewCell {
+    
+    
+    @IBOutlet var magazineImageView: UIImageView!
+    @IBOutlet var magazineDateLB: UILabel!
+    @IBOutlet var magazineSubTitleLB: UILabel!
+    @IBOutlet var magazineTitleLB: UILabel!
+    
+    func SetUpconfigure(title: String, subtitle: String, date: String, imageUrl: String) {
+        // 제목 레이블 설정
+        magazineTitleLB.numberOfLines = 0
+        magazineTitleLB.text = title
+        magazineTitleLB.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+      
+        
+        // 부제목 레이블 설정
+        magazineSubTitleLB.text = subtitle
+        magazineSubTitleLB.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        magazineSubTitleLB.textColor = .gray
+        
+        // 날짜 레이블 설정
+        magazineDateLB.text = dateFormat(dateString: date)
+        magazineDateLB.textColor = .lightGray
+        magazineDateLB.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        
+        // 이미지 뷰 설정
+        if let url = URL(string: imageUrl) {
+            magazineImageView.kf.setImage(with: url)
+        }
+        magazineImageView.contentMode = .scaleAspectFill
+        magazineImageView.layer.cornerRadius = 12
+        magazineImageView.clipsToBounds = true
+    }
+    
+    // 날짜 형식 변환 함수
+    func dateFormat(dateString: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyMMdd"
+        
+        guard let date = dateFormatter.date(from: dateString) else { return "" }
+        dateFormatter.dateFormat = "yy년 M월 d일"
+        return dateFormatter.string(from: date)
+    }
+}
