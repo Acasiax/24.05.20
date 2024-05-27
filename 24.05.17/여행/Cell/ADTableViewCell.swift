364c1e82af186dcaf60556c94f123d7826e4251d
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-05-27 22:54:38 +0900

    인기도시 - 코드 정리

---
+++ 24.05.17/여행/Cell/ADTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ADTableViewCell.swift
+//  24.05.17
+//
+//  Created by 이윤지 on 5/27/24.
+//
+
+import UIKit
+
+class ADTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
